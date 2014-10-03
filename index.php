2b97e00eb42b0589d1cd800bc2b958e04c24ed74
guilherme lopes <guiajlopes@gmail.com>
2014-10-03 14:26:42 -0300

    Colocando Slim para rotas, twig para templates e bootstrap para formatacao

--- index.php
+++ index.php
@@ -1,96 +1,59 @@
 <?php
+include_once 'includes/HuffmanNode.inc';
+include_once  'includes/Huffman.inc';
+include_once 'includes/ClasseAuxiliar.inc';
+include_once 'vendor/autoload.php';
 
-include_once 'HuffmanNode.inc';
-include_once  'Huffman.inc';
+use TrabalhoGA\Includes\ClasseAuxiliar;
+use TrabalhoGA\Includes\Huffman;
 
-// Seta as constantes.
+\Slim\Slim::registerAutoloader();
+
+$loader = new Twig_loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+$app = new \Slim\Slim();
+
 define('FILE_PATH', 'texto.txt');
 
-$values = getDicionary(FILE_PATH);
-$huffman = new Huffman($values);
-$huffman->buildTree();
-$huffman->codify($huffman->getRoot());
+$app->get('/', function() use ($app, $twig){
+  echo $twig->render('index.html');
+});
 
+$app->get('/decode', function() use ($app, $twig) {
+  $codifyString = '';
 
-$root = $huffman->getRoot();
-
-$codify_array = $huffman->getCodify();
-$codifyFile = codifyFile(FILE_PATH, $codify_array, $values);
-
-$name = 'codify.txt';
-$file = fopen($name, 'w');
-fwrite($file, $codifyFile);
-fclose($file);
-
-echo '<h1> Arquivo codificado </h1>';
-echo '<div>';
-echo $codifyFile;
-echo '</div>';
-
-/**
- * Codifica string para transferencia.
- *
- * @param string $file_path
- *  Path do arquivo que deve ser codificado.
- * @param array $codify_array
- *  Array com as codificações.
- * @param array $dicionary
- *  Array com o dicionario das palavras lidas no arquivo.
- *
- * @return string
- *  Retorna uma String contendo o dicionario serializado mais o arquivo codificado.
- *
- * @see SplFileObject Class
- */
-function codifyFile($file_path, $codify_array, $dicionary) {
-  $codifed = serialize($dicionary);
-  // Faz a leitura do arquivo.
-  $file = new SplFileObject($file_path);
-  // Percorre linhas a linha do arquivo.
-  while (!$file->eof()) {
-    $line = $file->fgetss();
-    // Percorre letra a letra da linha.
-    for ($i = 0; $i < strlen($line); $i++) {
-      // $escreve = fwrite($fp, $codify_array[$line[$i]]);
-      $codifed .= $codify_array[$line[$i]];
-    }
-  }
-  return $codifed;
-}
-
-/**
- * Realiza a leitura de um arquivo e transfere para um array de char.
- *
- * @param string $file_path
- *   Caminho do arquivo a ser lido.
- *
- * @return array
- *   Um Array de char ordenado contendo o dicionário do arquivo e suas respectivas frequencias.
- *
- * @see SplFileObject Class
- */
-function getDicionary($file_path) {
-  // Array contendo as palavras.
-  $palavras = array();
-
-  // Faz a leitura do arquivo.
-  $file = new SplFileObject($file_path);
-  // Percorre linhas a linha do arquivo.
-  while (!$file->eof()) {
-    $line = $file->fgetss();
-    // Percorre letra a letra da linha.
-    for ($i = 0; $i < strlen($line); $i++) {
-      // Verifica se palavra já existe no dicionario.
-      if (isset($palavras[$line[$i]])) {
-        // Incrementa o contador da palavra.
-        $palavras[$line[$i]] = $palavras[$line[$i]] + 1;
-      }
-      else {
-        $palavras[$line[$i]] = 1;
-      }
-    }
+  $codifyFile = new \SplFileObject('codify.txt');
+  while (!$codifyFile->eof()) {
+    $line = $codifyFile->fgetss();
+    $codifyString .= $line;
   }
 
-  arsort($palavras);
-  return  $palavras;
-}
+  echo $twig->render('decode.html', array('body' => ClasseAuxiliar::decodeFile($codifyString)));
+});
+
+$app->get('/encode', function () use ($app, $twig) {
+  $classe_auxiliar = new ClasseAuxiliar(FILE_PATH);
+  $dicionary = $classe_auxiliar->getDicionary();
+  $huffman = new Huffman($dicionary);
+  $huffman->buildTree();
+  $huffman->codify($huffman->getRoot());
+
+  $root = $huffman->getRoot();
+
+  $codify_array = $huffman->getCodify();
+
+  $codifyFile = $classe_auxiliar->codifyFile($codify_array, $dicionary);
+
+  $name = 'codify.txt';
+  $file = fopen($name, 'w');
+  fwrite($file, $codifyFile);
+  fclose($file);
+
+  echo $twig->render('encode.html', array(
+    'arquivo' => $codifyFile,
+    'tamanho' => $classe_auxiliar->getValorMedio($codify_array),
+  ));
+});
+
+$app->run();
+
